d9c79a479573539e358333abe957972c6f810be9
Michael Tolly <miketolly@gmail.com>
2016-06-01 16:22:20 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'deps/mustache/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+$mustache = new Mustache_Engine;
+
+require_once 'deps/markdown/Michelf/Markdown.inc.php';
+use \Michelf\Markdown;
+
+$parts = array();
+foreach (explode('/', $_SERVER['REQUEST_URI']) as $part) {
+  if ($part !== '') {
+    $parts[] = $part;
+  }
+}
+
+$lessons = json_decode(file_get_contents('lessons.json'), true);
+
+if ($lessons === null) {
+  header('HTTP/1.1 500 Internal Server Error');
+  echo 'Internal error';
+  die;
+}
+
+if (count($parts) === 1) {
+  $url = $parts[0];
+  if (array_key_exists($url, $lessons)) {
+    $lesson = $lessons[$url];
+    $lesson['description'] = Markdown::defaultTransform($lesson['description']);
+    echo $mustache->render(file_get_contents('lesson.tpl'), $lesson);
+  } else {
+    header('HTTP/1.1 404 Not Found');
+    echo 'Not found';
+  }
+} else {
+  echo 'Welcome to Field Day Learn.';
+}
---
+++ lesson.tpl
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Field Day Lab: {{title}}</title>
+  <meta charset="utf-8">
+</head>
+<body>
+
+<h1>{{title}}</h1>
+<h2>{{author}}</h2>
+
+{{{description}}}
+
+</body>
+</html>
